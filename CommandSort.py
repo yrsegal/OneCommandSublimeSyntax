6c15595a301fedbb1119faaf94bfe1ed7d7bf702
yrsegal <yrsegal@gmail.com>
2015-09-02 08:18:29 -0400

    allow command regex to select anything it should be able to

--- CommandSort.py
+++ CommandSort.py
@@ -1,6 +1,6 @@
 import sublime, sublime_plugin, re
 
-selector_regex = re.compile(r"@[prae]\[[\w!_=,]*\]")
+selector_regex = re.compile(r"@[prae]\[[^\[\]]*\]")
 
 class MinecraftSortSelectorCommand(sublime_plugin.TextCommand):
 	def run(self, edit):
